24f3b0c6b38826ab06067cf63ebb3e29eae9a206
BALAJI BEDRE <89333765+Balajibedre@users.noreply.github.com>
2021-10-11 10:37:48 +0530

    Add files via upload
    git all commands

---
+++ gitpath.py
@@ -0,0 +1,56 @@
+## GIT COMMAND
+
+git-add
+# Add file contents to the index
+
+git-checkout
+# Switch branches or 
+
+git-clean
+# Remove untracked files from the working tree
+
+git-clone
+# Clone a repository into a new directory
+
+git-commit
+# Record changes to the repository
+
+
+git-diff
+# Show changes between commits, commit and working tree, etc
+
+
+git-init
+# Create an empty Git repository or reinitialize an existing one
+
+git-merge
+# Join two or more development histories together
+
+git-pull
+# Fetch from and integrate with another repository or a local branch
+
+git-push
+# Update remote refs along with associated objects
+
+git-status
+# Show the working tree status
+
+
+git-checkout
+# Copy files from the index to the working tree
+
+git-commit
+# Write and verify Git commit-graph files
+
+git-branch
+
+
+git-diff # "file path"
+
+git-checkout -b
+
+git- fetch
+
+git-resetsoft #"file name"
+
+git-resethard #"file name"
